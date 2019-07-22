38ca2cb0163ae81c03cb8513b694f8a3719d42a2
ParticleChef <demonichje@gmail.com>
2019-07-22 18:21:52 -0500

    fixed lookup table, 7/22 saved

---
+++ pulsar_devkit/simple_examples/HLSIPs/METhls/save_0722/MET.cpp
@@ -0,0 +1,81 @@
+#include "MET.h"
+#include <cmath>
+#include <cassert>
+#ifndef __SYNTHESIS__
+#include <cstdio>
+#endif
+
+//#define TotalN 3
+//#define NEVENT 1
+
+void MET_hw(  pt_t allPT_hw[TotalN], fixed10_t &missPT_hw,  etaphi_t allPhi_hw[TotalN],  fixed10_t &missPhi_hw ){
+//void MET_hw( ap_int<16> allPT[TotalN], ap_int<16> &missPT, ap_int<10> allPhi[TotalN], ap_int<10> &missPhi ) {
+	#pragma HLS pipeline II=2
+	#pragma HLS array_partition variable=allPT_hw complete // block factor=2
+	#pragma HLS array_partition variable=allPhi_hw complete // block factor=2
+
+	int i;
+	int j;
+
+	fixedXY_t totalX = 0;
+	fixedXY_t totalY = 0;
+	fixed5_t co = 0;
+	fixed5_t si = 0;
+	fixed10_t pt = 0;
+
+//	std::cout<<"hw  : beform loop phi: "<<allPhi_hw [0]<<", "<<allPhi_hw [1]<<std::endl;
+//	std::cout<<"hw  : beform loop pt: "<<allPT_hw [0]<<", "<<allPT_hw [1]<<std::endl;
+		//std::cout<<"hw  : pt  --> "; for(int i = 0; i <TotalN-1; i++) std::cout<<allPT_hw[i]<<", "; std::cout<<allPT_hw[TotalN-1]<<std::endl;
+		//std::cout<<"hw  : phi --> "; for(int i = 0; i <TotalN-1; i++) std::cout<<allPhi_hw[i]<<", "; std::cout<<allPhi_hw[TotalN-1]<<std::endl;
+
+	for( i = 0; i < TotalN; i++){
+		//Cos<etaphi_t,double>(allPhi_hw[i],co);
+		//Sin<etaphi_t,double>(allPhi_hw[i],si);
+		Cos<etaphi_t,fixed5_t>(allPhi_hw[i],co);
+		Sin<etaphi_t,fixed5_t>(allPhi_hw[i],si);
+		//std::cout<<"hw  : sin,cos: "<<co<<", "<<si<<std::endl;
+		totalX = totalX -allPT_hw[i]*co;//s(allPhi_hw[i]);
+		totalY = totalY -allPT_hw[i]*si;//n(allPhi_hw[i]);
+		//std::cout<<"hw  : "<<"("<<i<<") "<<"totalX,Y: "<<totalX<<", "<<totalY<<std::endl;
+	}
+
+	Sqsqrt<fixedXY_t, fixed10_t>(totalX, totalY, pt);
+	std::cout<<"hw  : calculated MET: "<<pt<<std::endl;
+
+	missPT_hw = pt; // sqrt( totalX*totalX + totalY*totalY );
+	//std::cout<<"hw  : missPT_hw  = pt; "<<missPT_hw <<std::endl;
+
+	//double Res = 0;
+	//double res_phi = 0;
+	float divi = 0;
+	float res_phi = 0;
+	float degr = 57.32;
+	if( missPT_hw == 0 ){ 
+		divi = 1E+10; 
+		res_phi = 0; 
+	}
+	else{
+		divi = float( totalX/missPT_hw );
+		if(totalY < 0){acos<float, float>(divi, res_phi); res_phi = -1*res_phi;}
+		if(totalY >= 0) acos<float, float>(divi, res_phi);
+	}
+//	else{ 
+//		divi = float( totalX/missPT_hw );
+//		acos<float, float/*, 1024*/>(divi, res_phi );
+//		if(divi == 0 ){
+//			if(totalY == 0) res_phi = 0;
+//			if(totalY < 0) res_phi = -1*res_phi;
+//		}
+//		if(divi < 0) res_phi = -1*res_phi;;
+//		//std::cout<<"Res = "<<Res<<", result_phi = "<<res_phi<<std::endl;
+//	}
+
+	std::cout<<"hw  : totalX/missPT = "<<divi<<std::endl;
+//	division<etaphi_t, etaphi_t,N_TABLE_SIZE_NUM,N_TABLE_SIZE_DEN>(totalX, missPT_hw, Res);
+
+	//acos<double, double/*, 1024*/>(Res, res_phi );
+	missPhi_hw = res_phi*degr;//(180/3.14));
+	std::cout<<"hw  : acos(x/PT) = "<<missPhi_hw <<std::endl;
+//	missPhi_hw = acos( totalX / missPT_hw );
+
+}
---
+++ pulsar_devkit/simple_examples/HLSIPs/METhls/save_0722/MET.h
@@ -0,0 +1,202 @@
+#ifndef SIMPLE_ALGO_H
+#define SIMPLE_ALGO_H
+
+#include "ap_int.h"
+#include "ap_fixed.h"
+
+// size of the LUT
+#define N_TABLE_SIZE_NUM 1533 //Maximum number is 2045 for some reason (SIGSEGV otherwise)
+#define N_TABLE_SIZE_DEN 1533 //Maximum number is 2045 for some reason (SIGSEGV otherwise)
+#define N_TABLE_SIZE 1024
+#define SINE_RANGE 2
+
+typedef ap_int<16> pt_t;
+typedef ap_int<10> etaphi_t;
+//typedef ap_uint<16> val_t;
+
+// Type used for LUT (ap_fixed<X,Y>)
+#define AP_FIXED_SIZE 16
+#define AP_FIXED_DEC 12
+typedef ap_fixed<AP_FIXED_SIZE,AP_FIXED_DEC> fixed10_t;
+typedef ap_fixed<16,5> fixed5_t;
+typedef ap_fixed<24,13> fixedXY_t;
+
+//typedef ap_fixed<AP_FIXED_SIZE,AP_FIXED_DEC> pt_t;
+//typedef ap_fixed<AP_FIXED_SIZE,AP_FIXED_DEC> etaphi_t;
+
+#define TotalN 10
+#define NEVENT 10000
+
+//fixed10_t t_PI = 57.32; // 180/3.14
+//void MET_ref( ap_int<16> allPT_ref[TotalN], ap_int<16> &missPT_ref, ap_int<10> allPhi_ref[TotalN], ap_int<10> &missPhi_ref );
+//void MET_hw(  ap_int<16> allPT_hw[TotalN], ap_int<16> &missPT_hw, ap_int<10> allPhi_hw[TotalN], ap_int<10> &missPhi_hw );
+
+//void MET_ref( pt_t allPT_ref[TotalN], fixed10_t &missPT_ref, etaphi_t allPhi_ref[TotalN], fixed10_t &missPhi_ref );
+void MET_ref( double allPT_ref[TotalN], double &missPT_ref, double allPhi_ref[TotalN], double &missPhi_ref );
+void MET_hw(  pt_t allPT_hw[TotalN],  fixed10_t &missPT_hw,  etaphi_t allPhi_hw[TotalN],  fixed10_t &missPhi_hw );
+
+/**/
+// *************************************************
+//       Acosine
+// *************************************************
+template<class data_T, int N_TABLE>
+void init_acos_table(data_T table_out[N_TABLE]) {
+	int INDEX = 0;
+	//	Implement tanh lookup
+//	for (int ii = 0; ii < N_TABLE; ii++) {
+	for (int ii = N_TABLE-1; ii > -1; ii--) {
+		// Original: 
+		// First, convert from table index to X-value (signed 8-bit, range -4 to +4)
+		//float in_val = 2*4.0*(ii-float(N_TABLE)/2.0)/float(N_TABLE);
+
+		// Convert from table index to X-value (unsigned 4-bit, range 0 to +4)
+		float in_val = (SINE_RANGE)*((N_TABLE-1)-ii)/float(N_TABLE) - 1;
+
+		// Next, compute lookup table function
+		data_T real_val = acos(in_val);
+		//std::cout<<"("<<INDEX<<")in_val = "<<in_val<<", real_val = "<<real_val<<std::endl;
+		table_out[INDEX] = real_val;
+		INDEX++;
+	}
+	return;
+}
+template<class data_T, class res_T, int TABLE_SIZE>//=1024>
+void acos(data_T &data, res_T &res) {
+	// Initialize the lookup table
+	res_T acos_table[TABLE_SIZE];
+	init_acos_table<res_T, TABLE_SIZE>(acos_table);
+
+	// Index into the lookup table based on data
+	data_T datareg;
+	int index;
+
+#pragma HLS PIPELINE
+
+	//index = (1-data/SINE_RANGE)*TABLE_SIZE; // original
+	index = ((data+1)/SINE_RANGE)*TABLE_SIZE;
+	std::cout<<"index = : "<<index<<std::endl;
+
+	if (index < 0) index = 0;
+	if (index > TABLE_SIZE-1) index = TABLE_SIZE-1;
+	res = acos_table[index];
+	std::cout<<"acos_table[index] = "<<res<<std::endl;
+
+	return;
+}
+
+// Default table size provided here:
+template<class data_T, class res_T>
+void acos(data_T &data, res_T &res) { 
+	// Get the tanh value from the LUT 
+	//if (data < 0) {
+		//data = -1*data;  // original
+	//	data = data + 1;
+		acos<data_T, res_T, N_TABLE_SIZE>(data, res); 
+	//	res  = -1*res;
+	//}
+	//else{
+	//	data = data + 1;
+	//	acos<data_T, res_T, N_TABLE_SIZE>(data, res); 
+	//}//
+	return;
+}
+
+// *************************************************
+//       Math tables
+// *************************************************
+//template<class data_T, int N_TABLE>
+//void init_cos_table(data_T table_out[N_TABLE]) {
+//	//	Implement cos lookup
+//	for (int ii = 0; ii < N_TABLE; ii++) {
+//		// Convert from table index to X-value (unsigned 4-bit, range 0 to +4)
+//		float in_val = (SINE_RANGE)*((N_TABLE-1)-ii)/float(N_TABLE);
+//
+//		// Next, compute lookup table function
+//		data_T real_val = cos(in_val);
+//		table_out[ii] = real_val;
+//	}
+//	return;
+//}
+//template<class data_T, class res_T, int TABLE_SIZE>//=1024>
+//void Cos(data_T &data, res_T &res) {
+//	// Initialize the lookup table
+//	res_T cos_table[TABLE_SIZE];
+//	init_cos_table<res_T, TABLE_SIZE>(cos_table);
+//
+//	// Index into the lookup table based on data
+//	data_T datareg;
+//	int index;
+//
+//#pragma HLS PIPELINE
+//
+//	index = (1-data/SINE_RANGE)*TABLE_SIZE;
+//	std::cout<<"index = "<<index<<std::endl;
+//
+//	if (index < 0) index = 0;
+//	if (index > TABLE_SIZE-1) index = TABLE_SIZE-1;
+//	res = cos_table[index];
+//	std::cout<<"cos_table[index] = "<<res<<std::endl;
+//
+//	return;
+//}
+
+template<typename T_co, typename rT_co>
+void Cos(T_co &Inp, rT_co &Out){
+	double Input_co = 0.;
+	Input_co = Inp * (M_PI / 180);
+	//std::cout<<"Input_cos: "<<Input_co<<std::endl;
+	Out = cos(Input_co);
+}
+
+////// Sin table /////
+//template<class data_T, int N_TABLE>
+//void init_sin_table(data_T table_out[N_TABLE]) {
+//	//	Implement cos lookup
+//	for (int ii = 0; ii < N_TABLE; ii++) {
+//		// Convert from table index to X-value (unsigned 4-bit, range 0 to +4)
+//		float in_val = (SINE_RANGE)*((N_TABLE-1)-ii)/float(N_TABLE);
+//
+//		// Next, compute lookup table function
+//		data_T real_val = sin(in_val);
+//		table_out[ii] = real_val;
+//	}
+//	return;
+//}
+//template<class data_T, class res_T, int TABLE_SIZE>//=1024>
+//void Sin(data_T &data, res_T &res) {
+//	// Initialize the lookup table
+//	res_T sin_table[TABLE_SIZE];
+//	init_sin_table<res_T, TABLE_SIZE>(sin_table);
+//
+//	// Index into the lookup table based on data
+//	data_T datareg;
+//	int index;
+//
+//#pragma HLS PIPELINE
+//
+//	index = (1-data/SINE_RANGE)*TABLE_SIZE;
+//
+//	if (index < 0) index = 0;
+//	if (index > TABLE_SIZE-1) index = TABLE_SIZE-1;
+//	res = sin_table[index];
+//
+//	return;
+//}
+template<typename T_si, typename rT_si>
+void Sin(T_si &Inp, rT_si &Out){
+	double Input_si = 0.;
+	Input_si = Inp * (M_PI / 180);
+	//std::cout<<"Input_sin: "<<Input_si<<std::endl;
+	Out = sin(Input_si);
+}
+
+template<typename in_T, typename ou_T>
+void Sqsqrt(in_T &Inp1, in_T &Inp2, ou_T &Out){
+	double Out_res = 0.;
+	Out_res = Inp1*Inp1 + Inp2*Inp2;
+	Out =  sqrt(Out_res);
+}
+
+/**/
+
+#endif
