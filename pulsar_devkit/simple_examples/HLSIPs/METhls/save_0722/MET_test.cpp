06ada9d417fd34f5b61517d08a5d0ea1f1812c7d
ParticleChef <demonichje@gmail.com>
2019-07-24 09:46:42 -0500

    add random function

--- pulsar_devkit/simple_examples/HLSIPs/METhls/save_0722/MET_test.cpp
+++ pulsar_devkit/simple_examples/HLSIPs/METhls/save_0722/MET_test.cpp
@@ -10,6 +10,33 @@
 //#define TotalN 2
 //#define NEVENT 2
 
+#define IA 16807
+#define IM 2147483647
+#define AM (1.0/IM)
+#define IQ 127773
+#define IR 2836
+#define MASK 123459876
+
+float rand_input(long *idum)
+{
+	long k;
+	float ans;
+
+	*idum ^= MASK;
+	k=(*idum)/IQ;
+	*idum=IA*(*idum-k*IQ)-IR*k;
+	if (*idum < 0) *idum += IM; 
+	ans=AM*(*idum);
+	*idum ^= MASK;
+	return ans;
+}
+#undef IA
+#undef IM
+#undef AM
+#undef IQ
+#undef IR
+#undef MASK
+
 int main() {
 	//std::ofstream METout;
 	//std::ofstream PHIout;
@@ -22,12 +49,18 @@ int main() {
 	FILE *PHIout_;
 	FILE *METrefout;
 	FILE *PHIrefout;
+	FILE *RANDdist;
+	FILE *PTin;
+	FILE *PHIin;
 	METout=fopen("MET_out.txt","w");
 	PHIout=fopen("PHI_out.txt","w");
 	METout_=fopen("MET_out_.txt","w");
 	PHIout_=fopen("PHI_out_.txt","w");
 	METrefout=fopen("METref_out.txt","w");
 	PHIrefout=fopen("PHIref_out.txt","w");
+	RANDdist=fopen("RAND_dist.txt","w");
+	PTin=fopen("PT_in.txt","w");
+	PHIin=fopen("PHI_in.txt","w");
 
 	double allPT_ref[TotalN];
 	double missPT_ref;
@@ -39,22 +72,28 @@ int main() {
 	etaphi_t allPhi_hw[TotalN];
 	fixed10_t missPhi_hw;
 
-	int input_pt[TotalN] = {0}; // {10, 12};
-	int input_ph[TotalN] = {0}; // {85, -25};
+	double input_pt[TotalN] = {0}; // {10, 12};
+	double input_ph[TotalN] = {0}; // {85, -25};
 
+	long n = 1;
 	srand((unsigned int)time(NULL));
 	for (int test = 0; test < NEVENT; ++test) {
 		std::cout<<"==== NEVENT "<<test+1<<" ===="<<std::endl;
 
 		for(int nn = 0; nn < TotalN; ++nn){
-			//srand((unsigned int)time(NULL));
-			input_pt[nn] = rand()%291 + 10; //10 ~ 300
-			input_ph[nn] = rand()%360 - 180; // -180 ~ 180
+			fprintf(RANDdist, "%f\n", rand_input(&n));
+			//input_pt[nn] = rand()%291 + 10; //10 ~ 300
+			//input_ph[nn] = rand()%360 - 180; // -180 ~ 180
+			input_pt[nn] = rand_input(&n)*290.+10.;//%291 + 10; //10 ~ 300
+			input_ph[nn] = rand_input(&n)*360.-180.;//ran0(n);//%360 - 180; // -180 ~ 180
 
-			allPT_hw[nn] = input_pt[nn];
+			fprintf(PTin, "%f\n", input_pt[nn]);
+			fprintf(PHIin, "%f\n", input_ph[nn]);
+
+			allPT_hw[nn] =  input_pt[nn];
 			allPhi_hw[nn] = input_ph[nn];
 
-			allPT_ref[nn] = input_pt[nn];
+			allPT_ref[nn] =  input_pt[nn];
 			allPhi_ref[nn] = input_ph[nn];
 		}
 //		printf("test pt --> %d, %d\n",  allPT_ref[0],  allPT_ref[1] );// int(allPT_ref[0]),  int(allPT_ref[1]) );
@@ -116,6 +155,9 @@ int main() {
 	fclose(PHIout_);
 	fclose(METrefout);
 	fclose(PHIrefout);
+	fclose(RANDdist);
+	fclose(PTin);
+	fclose(PHIin);
 
 	return 0;
 }
