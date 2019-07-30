30d1439eeea3145b06bbab27806df1f40b64bdca
ParticleChef <demonichje@gmail.com>
2019-07-30 13:08:21 -0500

    sin and cos lookup tables and change II

--- pulsar_devkit/simple_examples/HLSIPs/METhls/MET.h
+++ pulsar_devkit/simple_examples/HLSIPs/METhls/MET.h
@@ -8,100 +8,47 @@
 #define N_TABLE_SIZE_NUM 1533 //Maximum number is 2045 for some reason (SIGSEGV otherwise)
 #define N_TABLE_SIZE_DEN 1533 //Maximum number is 2045 for some reason (SIGSEGV otherwise)
 #define N_TABLE_SIZE 1024
-#define SINE_RANGE 4
+#define SINE_RANGE 2
 
 typedef ap_int<16> pt_t;
 typedef ap_int<10> etaphi_t;
+//typedef ap_fixed<16,10> pt_t;
+//typedef ap_fixed<16,10> etaphi_t;
 
 // Type used for LUT (ap_fixed<X,Y>)
-#define AP_FIXED_SIZE 16
-#define AP_FIXED_DEC 10
-//typedef ap_uint<16> val_t;
-typedef ap_fixed<AP_FIXED_SIZE,AP_FIXED_DEC> fixed10_t;
+typedef ap_fixed<16,12> fixed10_t;
 typedef ap_fixed<16,5> fixed5_t;
+typedef ap_fixed<24,13> fixedXY_t;
 
-//typedef ap_fixed<AP_FIXED_SIZE,AP_FIXED_DEC> pt_t;
-//typedef ap_fixed<AP_FIXED_SIZE,AP_FIXED_DEC> etaphi_t;
 
 #define TotalN 2
-#define NEVENT 1
+#define NEVENT 5
 
-//void MET_ref( ap_int<16> allPT_ref[TotalN], ap_int<16> &missPT_ref, ap_int<10> allPhi_ref[TotalN], ap_int<10> &missPhi_ref );
-//void MET_hw(  ap_int<16> allPT_hw[TotalN], ap_int<16> &missPT_hw, ap_int<10> allPhi_hw[TotalN], ap_int<10> &missPhi_hw );
+#define PI_VAL 3.1415
 
-void MET_ref( pt_t allPT_ref[TotalN], pt_t &missPT_ref, etaphi_t allPhi_ref[TotalN], fixed10_t &missPhi_ref );
-void MET_hw(  pt_t allPT_hw[TotalN],  pt_t &missPT_hw,  etaphi_t allPhi_hw[TotalN],  fixed10_t &missPhi_hw );
+void MET_ref( double allPT_ref[TotalN], double &missPT_ref, double allPhi_ref[TotalN], double &missPhi_ref );
+void MET_hw(  pt_t allPT_hw[TotalN],  fixed10_t &missPT_hw,  etaphi_t allPhi_hw[TotalN],  fixed10_t &missPhi_hw );
 
-// *************************************************
-//       Division
-// *************************************************
-/**/ 
-template<class data_T, int N_TABLE_NUM, int N_TABLE_DEN>
-void init_division_table(data_T table_out[N_TABLE_NUM*N_TABLE_DEN]) {
-    // Implement division lookup
-    for (int inum = 0; inum < N_TABLE_NUM; inum++) {
-        for (int iden = 0; iden < N_TABLE_DEN; iden++) {
-            int index = (inum*N_TABLE_NUM)+iden;
-            // Compute lookup table function
-            data_T real_val = (iden>0) ? float(inum)/iden : 0;
-            table_out[index] = real_val;
-        }
-    }
-    return;
-}
 
-template<class data_T, class res_T, int TABLE_SIZE_NUM, int TABLE_SIZE_DEN>
-void division(data_T &data_num, data_T &data_den, res_T &res) {
-    // Initialize the lookup table
-    res_T division_table[TABLE_SIZE_NUM*TABLE_SIZE_DEN];
-    init_division_table<res_T, TABLE_SIZE_NUM, TABLE_SIZE_DEN>(division_table);
-
-    // Index into the lookup table based on data
-    int index_num, index_den, index;
-
-    //#pragma HLS PIPELINE
-
-    if (data_num < 0) data_num = 0;
-    if (data_den < 0) data_den = 0;
-    if (data_num > TABLE_SIZE_NUM-1) data_num = TABLE_SIZE_NUM-1;
-    if (data_den > TABLE_SIZE_DEN-1) data_den = TABLE_SIZE_DEN-1;
-    index = (data_num*TABLE_SIZE_NUM) + data_den;
-    res = division_table[index];
-
-    return;
-}
-
-// Default table size provided here:
-template<class data_T, class res_T>
-void division(data_T &data_num, data_T &data_den, res_T &res) { 
-    // Get the division value from the LUT 
-    if(data_den==0) {
-        std::cout << "WARNING::division::data_den==0" << std::endl;
-        return;
-    }
-    division<data_T, res_T, N_TABLE_SIZE_NUM, N_TABLE_SIZE_DEN>(data_num, data_den, res); 
-    return;
-}
-/**/
 /**/
 // *************************************************
-//       Acosine
+//       Accosine
 // *************************************************
 template<class data_T, int N_TABLE>
 void init_acos_table(data_T table_out[N_TABLE]) {
+	int INDEX = 0;
 	//	Implement tanh lookup
-	for (int ii = 0; ii < N_TABLE; ii++) {
-		// Original: 
-		// First, convert from table index to X-value (signed 8-bit, range -4 to +4)
-		//float in_val = 2*4.0*(ii-float(N_TABLE)/2.0)/float(N_TABLE);
+//	for (int ii = 0; ii < N_TABLE; ii++) {
+	for (int ii = N_TABLE-1; ii > -1; ii--) {
 
 		// Convert from table index to X-value (unsigned 4-bit, range 0 to +4)
-		float in_val = (SINE_RANGE)*((N_TABLE-1)-ii)/float(N_TABLE);
+		float in_val = (SINE_RANGE)*((N_TABLE-1)-ii)/float(N_TABLE) - 1;
 
 		// Next, compute lookup table function
 		data_T real_val = acos(in_val);
-		//std::cout << "Tanh:  Lookup table Index: " <<  ii<< " In Value: " << in_val << " Result: " << real_val << std::endl;
-		table_out[ii] = real_val;
+		//std::cout<<"("<<INDEX<<")in_val = "<<in_val<<", real_val = "<<real_val<<std::endl;
+		table_out[INDEX] = real_val;
+		INDEX++;
 	}
 	return;
 }
@@ -112,19 +59,17 @@ void acos(data_T &data, res_T &res) {
 	init_acos_table<res_T, TABLE_SIZE>(acos_table);
 
 	// Index into the lookup table based on data
-	data_T datareg;
 	int index;
 
 #pragma HLS PIPELINE
-	// Original:
-	//data_round = data.read()*TABLE_SIZE/8; // original 8-bit
-	//index = data_round + 4*TABLE_SIZE/8;   // original 8-bit (makes value positive)
-
-	index = (1-data/SINE_RANGE)*TABLE_SIZE;
+	//index = (1-data/SINE_RANGE)*TABLE_SIZE; // original
+	index = ((data+1)/SINE_RANGE)*TABLE_SIZE;
+	std::cout<<"index = : "<<index<<std::endl;
 
 	if (index < 0) index = 0;
 	if (index > TABLE_SIZE-1) index = TABLE_SIZE-1;
 	res = acos_table[index];
+	std::cout<<"acos_table[index] = "<<res<<std::endl;
 
 	return;
 }
@@ -132,39 +77,129 @@ void acos(data_T &data, res_T &res) {
 // Default table size provided here:
 template<class data_T, class res_T>
 void acos(data_T &data, res_T &res) { 
-	// Get the tanh value from the LUT 
-	if (data < 0) {
-		data = -1*data;
+	// Get the acos value from the LUT 
 		acos<data_T, res_T, N_TABLE_SIZE>(data, res); 
-		res  = -1*res;
-	}
-	else{
-		acos<data_T, res_T, N_TABLE_SIZE>(data, res); 
-	}
 	return;
 }
 
-template<typename T_co, typename rT_co>
-void Cos(T_co &Inp, rT_co &Out){
-	double Input_co = 0.;
-	Input_co = Inp * (M_PI / 180);
-	std::cout<<"Input_cos: "<<Input_co<<std::endl;
-	Out = cos(Input_co);
+// *************************************************
+//       Cosine tables
+// *************************************************
+template<class rT_co, int N_TABLE>
+void init_cos_table(rT_co table_out[N_TABLE]) {
+	int INDEX = 0;
+	//	Implement cos lookup
+	for (int ii = N_TABLE-1; ii > -1; ii--) {
+		// Convert from table index to X-value
+		float in_val = ((2*PI_VAL)*((N_TABLE-1)-ii)/float(N_TABLE)) - PI_VAL;
+
+		// Next, compute lookup table function
+		rT_co real_val = cos(in_val);
+		table_out[INDEX] =  real_val;
+		//std::cout<<"("<<INDEX<<") cos("<<in_val<<") = "<<real_val<<std::endl;
+		INDEX++;
+	}
+	return;
+}
+template<class T_co, class rT_co, int TABLE_SIZE>
+void cos(T_co &data, rT_co &res) {
+	// Initialize the lookup table
+	rT_co cos_table[TABLE_SIZE];
+	init_cos_table<rT_co, TABLE_SIZE>(cos_table);
+
+	// Index into the lookup table based on data
+	int index;
+
+#pragma HLS PIPELINE II = 4
+
+	double data_rad = data * 0.0174; // (PI_VAL/180);
+	std::cout<<"data = "<<data_rad<<std::endl;
+	index = ((data_rad+PI_VAL)/(2*PI_VAL))*TABLE_SIZE;
+	std::cout<<"cos index = "<<index<<std::endl;
+
+	if (index < 0) index = 0;
+	if (index > TABLE_SIZE-1) index = TABLE_SIZE-1;
+	res = cos_table[index];
+	std::cout<<"cos_table[index] = "<<res<<std::endl;
+
+	return;
 }
 
-template<typename T_si, typename rT_si>
-void Sin(T_si &Inp, rT_si &Out){
-	double Input_si = 0.;
-	Input_si = Inp * (M_PI / 180);
-	std::cout<<"Input_sin: "<<Input_si<<std::endl;
-	Out = sin(Input_si);
+template<class T_co, class rT_co>
+void cos(T_co &Inp, rT_co &Out){
+	//double Input_co = 0.;
+	//Input_co = Inp * (M_PI / 180);
+	cos<T_co, rT_co, N_TABLE_SIZE>(Inp, Out);
+	//std::cout<<"Input_cos: "<<Input_co<<std::endl;
+	//Out = cos(Input_co);
 }
+
+// *************************************************
+//       Sine tables
+// *************************************************
+template<class rT_co, int N_TABLE>
+void init_sin_table(rT_co table_out[N_TABLE]) {
+	int INDEX = 0;
+	//	Implement cos lookup
+	for (int ii = N_TABLE-1; ii > -1; ii--) {
+		// Convert from table index to X-value
+		float in_val = ((2*PI_VAL)*((N_TABLE-1)-ii)/float(N_TABLE)) - PI_VAL;
+
+		// Next, compute lookup table function
+		rT_co real_val = sin(in_val);
+		table_out[INDEX] = real_val;
+		//std::cout<<"("<<INDEX<<") cos("<<in_val<<") = "<<real_val<<std::endl;
+		INDEX++;
+	}
+	return;
+}
+template<class T_co, class rT_co, int TABLE_SIZE>
+void sin(T_co &data, rT_co &res) {
+	// Initialize the lookup table
+	rT_co sin_table[TABLE_SIZE];
+	init_sin_table<rT_co, TABLE_SIZE>(sin_table);
+
+	// Index into the lookup table based on data
+	int index;
+
+#pragma HLS PIPELINE II = 4
+
+	double data_rad = data * 0.0174; // (PI_VAL/180);
+	std::cout<<"data = "<<data_rad<<std::endl;
+	index = ((data_rad+PI_VAL)/(2*PI_VAL))*TABLE_SIZE;
+	std::cout<<"sin index = "<<index<<std::endl;
+
+	if (index < 0) index = 0;
+	if (index > TABLE_SIZE-1) index = TABLE_SIZE-1;
+	res = sin_table[index];
+	std::cout<<"sin_table[index] = "<<res<<std::endl;
+
+	return;
+}
+
+template<class T_co, class rT_co>
+void sin(T_co &Inp, rT_co &Out){
+	//double Input_co = 0.;
+	//Input_co = Inp * (M_PI / 180);
+	sin<T_co, rT_co, N_TABLE_SIZE>(Inp, Out);
+	//std::cout<<"Input_cos: "<<Input_co<<std::endl;
+	//Out = cos(Input_co);
+}
+
+
+//template<typename T_si, typename rT_si>
+//void Sin(T_si &Inp, rT_si &Out){
+//	double Input_si = 0.;
+//	Input_si = Inp * (M_PI / 180);
+//	//std::cout<<"Input_sin: "<<Input_si<<std::endl;
+//	Out = sin(Input_si);
+//}
 
 template<typename in_T, typename ou_T>
 void Sqsqrt(in_T &Inp1, in_T &Inp2, ou_T &Out){
 	double Out_res = 0.;
 	Out_res = Inp1*Inp1 + Inp2*Inp2;
-	Out = round( sqrt(Out_res));
+	Out =  sqrt(Out_res);
 }
 
 /**/
--- pulsar_devkit/simple_examples/HLSIPs/METhls/MET_test.cpp
+++ pulsar_devkit/simple_examples/HLSIPs/METhls/MET_test.cpp
@@ -1,4 +1,7 @@
 #include <cstdio>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
 #include "MET.h"
 #include "ap_int.h"
 
@@ -7,44 +10,99 @@
 //#define TotalN 2
 //#define NEVENT 2
 
-int main() {
+#define IA 16807
+#define IM 2147483647
+#define AM (1.0/IM)
+#define IQ 127773
+#define IR 2836
+#define MASK 123459876
 
-	pt_t allPT_ref[TotalN];
-	pt_t missPT_ref;
-	etaphi_t allPhi_ref[TotalN];
-	fixed10_t missPhi_ref;
+float rand_input(long *idum)
+{
+	long k;
+	float ans;
+
+	*idum ^= MASK;
+	k=(*idum)/IQ;
+	*idum=IA*(*idum-k*IQ)-IR*k;
+	if (*idum < 0) *idum += IM; 
+	ans=AM*(*idum);
+	*idum ^= MASK;
+	return ans;
+}
+#undef IA
+#undef IM
+#undef AM
+#undef IQ
+#undef IR
+#undef MASK
+
+int main() {
+	//std::ofstream METout;
+	//std::ofstream PHIout;
+	//METout.open("./MET_out.txt");
+	//PHIout.open("./PHI_out.txt");
+
+	FILE *METout;
+	FILE *PHIout;
+	FILE *METout_;
+	FILE *PHIout_;
+	FILE *METrefout;
+	FILE *PHIrefout;
+	FILE *RANDdist;
+	FILE *PTin;
+	FILE *PHIin;
+	METout=fopen("MET_out.txt","w");
+	PHIout=fopen("PHI_out.txt","w");
+	METout_=fopen("MET_out_.txt","w");
+	PHIout_=fopen("PHI_out_.txt","w");
+	METrefout=fopen("METref_out.txt","w");
+	PHIrefout=fopen("PHIref_out.txt","w");
+	RANDdist=fopen("RAND_dist.txt","w");
+	PTin=fopen("PT_in.txt","w");
+	PHIin=fopen("PHI_in.txt","w");
+
+	double allPT_ref[TotalN];
+	double missPT_ref;
+	double allPhi_ref[TotalN];
+	double missPhi_ref;
 
 	pt_t allPT_hw[TotalN];
-	pt_t missPT_hw;
+	fixed10_t missPT_hw;
 	etaphi_t allPhi_hw[TotalN];
 	fixed10_t missPhi_hw;
 
+	double input_pt[TotalN] = {0}; // {10, 12};
+	double input_ph[TotalN] = {0}; // {85, -25};
+
+	long n = 2;
+	srand((unsigned int)time(NULL));
 	for (int test = 0; test < NEVENT; ++test) {
+		std::cout<<"==== NEVENT "<<test+1<<" ===="<<std::endl;
 
-		allPT_hw[0] = 4+test;
-		allPT_hw[1] = 4+test;
-//		allPT_hw[2] = -8+test;
+		for(int nn = 0; nn < TotalN; ++nn){
+			fprintf(RANDdist, "%f\n", rand_input(&n));
+			//input_pt[nn] = rand()%291 + 10; //10 ~ 300
+			//input_ph[nn] = rand()%360 - 180; // -180 ~ 180
+			input_pt[nn] = rand_input(&n)*290.+10.;//%291 + 10; //10 ~ 300
+			input_ph[nn] = rand_input(&n)*360.-180.;//ran0(n);//%360 - 180; // -180 ~ 180
 
-		allPhi_hw[0] = -90+test;
-		allPhi_hw[1] = 90+test;
-//		allPhi_hw[2] = 0.6+test;
+			fprintf(PTin, "%f\n", input_pt[nn]);
+			fprintf(PHIin, "%f\n", input_ph[nn]);
 
-		allPT_ref[0] = 4+test;
-		allPT_ref[1] = 4+test;
-//		allPT_ref[2] = -8+test;
+			allPT_hw[nn] =  round(input_pt[nn]);
+			allPhi_hw[nn] = round(input_ph[nn]);
 
-		allPhi_ref[0] = -90+test;
-		allPhi_ref[1] = 90+test;
-//		allPhi_ref[2] = 0.6+test;
-
+			allPT_ref[nn] =  round(input_pt[nn]);
+			allPhi_ref[nn] = round(input_ph[nn]);
+		}
 //		printf("test pt --> %d, %d\n",  allPT_ref[0],  allPT_ref[1] );// int(allPT_ref[0]),  int(allPT_ref[1]) );
 //		printf("test phi --> %d, %d\n", allPhi_ref[0], allPhi_ref[1] );//int(allPhi_ref[0]), int(allPhi_ref[1]) );
 
-		std::cout<<"test: pt  --> "<<allPT_ref[0]<<", "<<allPT_ref[1]<<std::endl;
-		std::cout<<"test: phi --> "<<allPhi_ref[0]<<", "<<allPhi_ref[1]<<std::endl;
-
-//		printf("input pt --> %f, %f\n",  allPT_ref[0],  allPT_ref[1] );
-//		printf("input phi --> %f, %f\n", allPhi_ref[0], allPhi_ref[1] );
+		std::cout<<"test: pT  --> "; for(int i = 0; i <TotalN-1; i++) std::cout<<allPT_ref[i]<<", "; std::cout<<allPT_ref[TotalN-1]<<std::endl;
+		std::cout<<"test: phi --> "; for(int i = 0; i <TotalN-1; i++) std::cout<<allPhi_ref[i]<<", "; std::cout<<allPhi_ref[TotalN-1]<<std::endl;
+		std::cout<<"test: pT  --> "; for(int i = 0; i <TotalN-1; i++) std::cout<<allPT_hw[i]<<", "; std::cout<<allPT_hw[TotalN-1]<<std::endl;
+		std::cout<<"test: phi --> "; for(int i = 0; i <TotalN-1; i++) std::cout<<allPhi_hw[i]<<", "; std::cout<<allPhi_hw[TotalN-1]<<std::endl;
 
 		int i;
 		int j;
@@ -54,11 +112,11 @@ int main() {
 
 		MET_ref(allPT_ref, missPT_ref, allPhi_ref, missPhi_ref);
 
-		std::cout<<"test: ref process"<<std::endl;
+		std::cout<<"test: ref process end"<<std::endl;
 
 		MET_hw( allPT_hw, missPT_hw, allPhi_hw, missPhi_hw);
 
-		std::cout<<"test: hw process"<<std::endl;
+		std::cout<<"test: hw process end"<<std::endl;
 
 //		printf( "test MET_ref = %d \n", missPT_ref ); // int(missPT_ref) );
 //		printf( "test Phi_ref = %d \n", missPhi_ref );//int(missPhi_ref) );
@@ -71,6 +129,20 @@ int main() {
 		std::cout<<"test: MET_hw = "<<missPT_hw<<std::endl;
 		std::cout<<"test: Phi_hw = "<<missPhi_hw<<std::endl;
 
+		//std::cout<<"MET_ref = "<<missPT_ref<<", MET_hw = "<<(double)missPT_hw<<std::endl;
+
+		//if((missPhi_hw < 0 || missPhi_hw > 90)||(missPhi_ref < 0|| missPhi_hw > 90)) continue;
+
+		double wrt_pT = missPT_ref - (double)missPT_hw;
+		double wrt_ph = missPhi_ref - (double)missPhi_hw;
+		if( wrt_ph > 180 ) wrt_ph = wrt_ph - 2*180;
+		if( wrt_ph <= -180)wrt_ph = wrt_ph + 2*180;
+		fprintf(METout, "%f\n", wrt_pT);
+		fprintf(PHIout, "%f\n", wrt_ph);
+		fprintf(METout_, "%f\n", wrt_pT/missPT_ref);
+		fprintf(PHIout_, "%f\n", wrt_ph/missPT_ref);
+		fprintf(METrefout, "%f\n", missPT_ref);
+		fprintf(PHIrefout, "%f\n", missPhi_ref);
 //		printf( "test MET_ref = %f \n", missPT_ref );
 //		printf( "test Phi_ref = %f \n", missPhi_ref );
 
@@ -78,6 +150,16 @@ int main() {
 //		printf( "test Phi_hw = %f \n", missPhi_hw );
 
 	}
+
+	fclose(METout);
+	fclose(PHIout);
+	fclose(METout_);
+	fclose(PHIout_);
+	fclose(METrefout);
+	fclose(PHIrefout);
+	fclose(RANDdist);
+	fclose(PTin);
+	fclose(PHIin);
 
 	return 0;
 }
